2addab9746d5c8659632598de024d6071441bec4
Sunhomie <79144124+Sunhomie@users.noreply.github.com>
2021-05-18 19:34:22 +0200

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,15 +1,19 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-
-public class GUI extends JFrame{
+public class GUI extends JFrame implements ActionListener{
 
 	
 	private JFrame frame = new JFrame();
 	private JPanel panel = new JPanel();
 	private JLabel label = new JLabel("Welcome to our exam application!");
 	private JButton b1, b2, b3, b4, b5;
-	
+	private JMenuBar menuBar;
+	private JMenu menu, menu2, menu3;
+	private JMenuItem menuItem1, menuItem2, menuItem3;
+	private JMenu subMenu;
+	private JCheckBoxMenuItem checkItem1, checkItem2;
 	
 	
 	public GUI() {
@@ -17,21 +21,24 @@ public class GUI extends JFrame{
 		super();
 		panel.setLayout(null);
 		
+		// Buttons
 		b1 = new JButton("Add customer");
 		b2 = new JButton("Listing orders");
 		b3 = new JButton("Retrieve all employees");
 		b4 = new JButton("Bulk import from file");
 		b5 = new JButton("Exit");
 		
+		// Button placement
 		b1.setBounds(50, 100, 200, 70);
 		b2.setBounds(300, 100, 200, 70);
 		b3.setBounds(50, 220, 200, 70);
 		b4.setBounds(300, 220, 200, 70);
 		b5.setBounds(175, 350, 200, 40);
 		
+		// Label placement
 		label.setBounds(185, 40, 400, 50);
 		
-		
+		// Adding buttons, label and border
 		panel.add(b1);
 		panel.add(b2);
 		panel.add(b3);
@@ -40,7 +47,7 @@ public class GUI extends JFrame{
 		panel.add(label);
 		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
 		
-		
+		// Frame values
 		frame.add(panel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Exam Application");
@@ -48,16 +55,68 @@ public class GUI extends JFrame{
 		frame.setResizable(false);
 		frame.setVisible(true);
 		
-	}
-	
-
-	
-	
-	public static void main(String[] args) {
-		new GUI();
+		// Menu Bar beginning
+		menuBar = new JMenuBar();
+		
+		// Visible menus
+		menu = new JMenu("File");
+		menuBar.add(menu);
+		
+		menu2 = new JMenu("Actions");
+		menuBar.add(menu2);
+		
+		menu3 = new JMenu("Help");
+		menuBar.add(menu3);
+		
+		// Menus within menus
+		menuItem1 = new JMenuItem("Load");
+		menuItem1.addActionListener(this);
+		menu.add(menuItem1);
+		
+		menuItem2 = new JMenuItem("Save");
+		menuItem2.addActionListener(this);
+		menu.add(menuItem2);
+		
+		menuItem3 = new JMenuItem("Options");
+		menuItem3.addActionListener(this);
+		menu.add(menuItem3);
+		
+		// Submenu within menus' menus
+		subMenu = new JMenu("Submenu");
+		menu.add(subMenu);
+		
+		// Checkbox items within submenus
+		checkItem1 = new JCheckBoxMenuItem("item #1");
+		checkItem1.addActionListener(this);
+		subMenu.add(checkItem1);
+		
+		checkItem2 = new JCheckBoxMenuItem("item #2");
+		checkItem2.addActionListener(this);
+		subMenu.add(checkItem2);
+		
+		// Menu Bar sizing 
+		frame.setSize(549, 500);
+		frame.setJMenuBar(menuBar);
 		
 	}
+			
+		
+	public static void main(String[] args) {
+		new GUI();
+	}
 
 
 
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 }
+		
+		
+		
+		
+		
+		
+
