cda264bf4a40cfa3dc6501bc3ffe71f963471142
RitaB-afk <77437092+RitaB-afk@users.noreply.github.com>
2021-05-19 12:39:07 +0200

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,87 @@
+package com.exam.OOPGroup14;
+
+	
+	public class Order {
+	    private int orderNumber;
+	    private String orderDate;
+	    private String requiredDate;
+	    private String shippedDate;
+	    private String status;
+	    private String comments;
+	    private int customerNumber;
+
+	    public Order (int orderNumber, String orderDate, String requiredDate, String shippedDate, String status, String comments, int customerNumber){
+	        super();
+	        this.orderNumber=orderNumber;
+	        this.orderDate= orderDate;
+	        this.requiredDate= requiredDate;
+	        this.shippedDate= shippedDate;
+	        this.status=status;
+	        this.comments=comments;
+	        this.customerNumber=customerNumber;
+
+	    }
+	    public int getOrderNumber() {
+	        return orderNumber;
+	    }
+
+	    public void setOrderNumber(int orderNumber) {
+	        this.orderNumber=orderNumber;
+	    }
+
+	    public String getOrderDate() {
+	        return orderDate;
+	    }
+
+	    public void setOrderDate(String orderDate) {
+	        this.orderDate= orderDate;
+	    }
+
+	    public String getRequiredDate() {
+	        return requiredDate;
+	    }
+
+	    public void setRequiredDate (String requiredDate) {
+	        this.requiredDate= requiredDate;
+	    }
+
+	    public String getShippedDate() {
+	        return shippedDate;
+	    }
+
+	    public void setShippedDate(String shippedDate) {
+	        this.shippedDate = shippedDate;
+	    }
+
+	    public String getStatus() {
+	        return status;
+	    }
+
+	    public void setStatus(String status) {
+	        this.status=status;
+	    }
+
+	    public String getComments() {
+	        return comments;
+	    }
+	    public void setComments(String comments){
+	        this.comments=comments;
+
+	    }
+	    public int getCustomerNumber() {
+	    	return customerNumber;
+	    	
+	    }
+	    
+	    public void setCustomerNumber(int customerNumber) {
+	    	this.customerNumber= customerNumber;
+	    }
+	    @Override
+	    public String toString() {
+	        return String
+	                .format("Order [orderNumber=%s, orderDate=%s, requiredDate=%s, shippedDate=%s, status=%s,comments=%s,customerNumber=%s)]",
+	                        orderNumber, orderDate, requiredDate, shippedDate, status, comments, customerNumber);
+	    }
+
+
+	}
